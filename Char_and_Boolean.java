12e04120559c1e1006621485958d7f04ae6f2af9
Aashi Dutt <aashi.dutt3@gmail.com>
2018-08-15 21:49:08 +0530

    Create Char_and_Boolean.java

---
+++ Char_and_Boolean.java
@@ -0,0 +1,19 @@
+// This code gives basic glimpse of character(char) and boolean datatype
+// refer to https://unicode-table.com/en/   for unicodes of  all characters
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  char Registered = '\u00AE';
+
+	  System.out.println("Registered sign is " + Registered );
+
+	  boolean IsMale = true;
+      if(IsMale == true);
+        System.out.println("He is a Male");
+
+
+    }
+}
