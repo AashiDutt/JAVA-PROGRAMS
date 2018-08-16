2946239aec9e4b9e6568d6ba2982d52d57d6046e
AashiDutt <aashi.dutt3@gmail.com>
2018-08-16 05:44:57 +0530

    new

--- Hello_World/src/hello.java
+++ Hello_World/src/hello.java
@@ -2,5 +2,16 @@ public class hello {
 
    public static void main(String[] args){
        System.out.println("Hello World!!");
+
+       int var_1 = 556; // declaring  variable
+       int var_2 = 710;
+       int var_3 = 100;
+
+       int output = var_1 + var_2 +var_3;
+       System.out.println("output = ");
+       System.out.println(output);
+
+       int sub = 1000 - output;
+       System.out.println(sub);
    }
 }
---
+++ bool/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   boolean var1 = true;
+	   if(var1 == true);
+        System.out.println("hello !!");
+    }
+}
