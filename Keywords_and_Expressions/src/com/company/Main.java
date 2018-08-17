131a7e3118c2ed6feb8cd861c10bca6a022fc304
AashiDutt <aashi.dutt3@gmail.com>
2018-08-17 22:30:37 +0530

    Update

---
+++ Keywords_and_Expressions/src/com/company/Main.java
@@ -0,0 +1,18 @@
+// This code helps in identifying Expressions and keywords(highlighted in blue)
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //  creating Expressions
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int high_score = 50;
+
+        if (high_score == 50) {  // the statement within brackets is an expression ..while whole line of code is a statement
+            System.out.println("this is an expression");
+        }
+
+    }
+}
