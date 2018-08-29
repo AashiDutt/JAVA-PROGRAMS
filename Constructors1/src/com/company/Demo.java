aa1e00ba2d0e415e91ad959c6b8e61090334f2be
AashiDutt <aashi.dutt3@gmail.com>
2018-08-29 20:56:15 +0530

    Constructor

---
+++ Constructors1/src/com/company/Demo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Demo {
+  // creating a Constructor
+  // A constructor has same name as that of class
+    Demo(){
+        System.out.println("This is a constructor");
+
+    }
+
+    void show(){
+        System.out.println("This is show method");
+    }
+}
---
+++ Constructors1/src/com/company/Main.java
@@ -0,0 +1,15 @@
+// code to implement constructors
+// A constructor has same name as that of class
+// A constructor is called when its object is created
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   Demo d = new Demo();  // creating object which utomatically calls a constructor
+        Demo d1 = new Demo();  // creating object for show  method and works to call constructor too
+        d1.show();    // calling show method
+
+    }
+}
